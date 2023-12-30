b3d876be440529657aba5e266538fe15fe4866cd
ovchinnikova <ov4innikova.e.a@mail.ru>
2023-12-30 12:26:23 +0300

    added main file

---
+++ src/main/java/pro/sky/java/course2/arraylistimpl/ArrayListImplApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.arraylistimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayListImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrayListImplApplication.class, args);
+	}
+
+}
