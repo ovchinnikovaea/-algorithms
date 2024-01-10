4d25cedc3c53a941f212d641c5e5ba1340967589
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-01-10 19:37:28 +0300

    homework1 is done

---
+++ src/main/java/pro/sky/java/course2/arraylistimpl/controller/ArrayListController.java
@@ -0,0 +1,4 @@
+package pro.sky.java.course2.arraylistimpl.controller;
+
+public class ArrayListController {
+}
---
+++ src/main/java/pro/sky/java/course2/arraylistimpl/service/ItemsIsFullException.java
@@ -0,0 +1,23 @@
+package pro.sky.java.course2.arraylistimpl.service;
+
+public class ItemsIsFullException extends RuntimeException {
+
+    public ItemsIsFullException() {
+    }
+
+    public ItemsIsFullException(String message) {
+        super(message);
+    }
+
+    public ItemsIsFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ItemsIsFullException(Throwable cause) {
+        super(cause);
+    }
+
+    public ItemsIsFullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
